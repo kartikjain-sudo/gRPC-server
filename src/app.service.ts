95bbf5ed545fc8a44fb836e93a156cf1fecff1a3
kartikjn <kj@rapidinnovation.dev>
2023-01-12 00:57:19 +0530

    draft: implementing logic

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  public accumulate(data: number[]): number {
+    return (data || []).reduce((a, b) => Number(a) + Number(b));
+}
 }
